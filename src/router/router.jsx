36bd5a19d990d4ecb1381a97ca148f263e65a663
kousar-21 <kousarahammed5@gmail.com>
2026-01-06 14:29:51 +0600

    adding home page in router

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,9 +1,16 @@
 import { createBrowserRouter } from "react-router";
 import MainLayout from "../Layout/MainLayout";
+import Home from "../Home/Home";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout></MainLayout>,
+    children:[
+      {
+        path: true,
+        element: <Home></Home>,
+      }
+    ]
   },
 ]);
