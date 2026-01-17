667195e010ff9a0e43844eb9511cd6254568efb5
kousar-21 <kousarahammed5@gmail.com>
2026-01-17 13:36:50 +0600

    added contact component in route

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router";
 import MainLayout from "../Layout/MainLayout";
 import Home from "../Pages/Home/Home";
 import AboutPage from "../Component/About";
+import Contact from "../Component/Contact";
 
 export const router = createBrowserRouter([
   {
@@ -15,6 +16,10 @@ export const router = createBrowserRouter([
       {
         path: "/about",
         element: <AboutPage></AboutPage>
+      },
+      {
+        path: "/contact",
+        element: <Contact></Contact>
       }
     ]
   },
