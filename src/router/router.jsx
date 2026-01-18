8a8923da61757e83e555f37648cbf73b46340969
kousar-21 <kousarahammed5@gmail.com>
2026-01-18 19:03:23 +0600

    added services component in route

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -3,6 +3,7 @@ import MainLayout from "../Layout/MainLayout";
 import Home from "../Pages/Home/Home";
 import AboutPage from "../Component/About";
 import Contact from "../Component/Contact";
+import Services from "../Component/Services";
 
 export const router = createBrowserRouter([
   {
@@ -20,6 +21,10 @@ export const router = createBrowserRouter([
       {
         path: "/contact",
         element: <Contact></Contact>
+      },
+      {
+        path: "/services",
+        element: <Services></Services>
       }
     ]
   },
