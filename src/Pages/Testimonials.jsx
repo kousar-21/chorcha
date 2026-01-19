365ade408376b66dcf0a478cbf94ae581da81c56
kousar-21 <kousarahammed5@gmail.com>
2026-01-19 13:33:54 +0600

    added testimonial component

---
+++ src/Pages/Testimonials.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Testimonials = () => {
+    return (
+        <div>
+            Testimonial component.
+        </div>
+    );
+};
+
+export default Testimonials;
