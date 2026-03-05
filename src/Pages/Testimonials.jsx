a62c53d18dac95726b8d4d35935dc7b68f1a6591
kousar-21 <kousarahammed5@gmail.com>
2026-03-05 14:49:48 +0600

    changed class in testimonials

--- src/Pages/Testimonials.jsx
+++ src/Pages/Testimonials.jsx
@@ -23,7 +23,7 @@ const Testimonials = () => {
   ];
 
   return (
-    <section className="py-16 md:py-20 bg-gradient-to-b from-gray-50 to-white">
+    <section className="py-16 md:py-20 bg-Linear-to-b from-gray-50 to-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         
         {/* Logo Cloud - Build Authority */}
