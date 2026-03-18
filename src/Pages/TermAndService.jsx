f8274b5183a973ed40734428aa90ee02b6d4fb60
kousar-21 <kousarahammed5@gmail.com>
2026-03-18 13:29:16 +0600

    added term and services page

---
+++ src/Pages/TermAndService.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TermAndService = () => {
+    return (
+        <div>
+            term and services page
+        </div>
+    );
+};
+
+export default TermAndService;
