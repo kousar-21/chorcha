379bb22d7d2cd5f47087bc9c81f67b08a604f7b0
kousar-21 <kousarahammed5@gmail.com>
2026-04-05 22:21:05 +0600

    added basic features page structure

---
+++ src/Pages/Features.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Features = () => {
+    return (
+        <div>
+            this will be features pages.
+            
+        </div>
+    );
+};
+
+export default Features;
