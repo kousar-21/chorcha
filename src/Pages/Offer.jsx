0b96721dd176cb80f564b45a02e57358ec7123d7
kousar-21 <kousarahammed5@gmail.com>
2026-05-09 20:16:52 +0600

    updated bg color in offer page

--- src/Pages/Offer.jsx
+++ src/Pages/Offer.jsx
@@ -91,7 +91,7 @@ const Offer = () => {
   };
 
   return (
-    <div className="min-h-screen bg-linear-to-br from-gray-50 to-gray-100 py-12 px-4 sm:px-6 lg:px-8">
+    <div className="min-h-screen bg-primary py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-7xl mx-auto">
         {/* Header Section */}
         <div className="text-center mb-16">
